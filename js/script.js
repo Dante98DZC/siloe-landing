0a5ba1f7e3d3cc1f3626528f51e0dcf3c9fa6ef5
David Zayas <dante98dzc@gmail.com>
2025-02-16 21:16:44 -0500

    Implement image gallery functionality with navigation; remove Galleria Classic plugin files and update styles for improved layout and responsiveness.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-// Nothing yet
+$(document).ready(function () {
+    let images = $('.gallery img');
+    let currentIndex = 0;
+
+    function showImage(index) {
+        images.removeClass('active');
+        $(images[index]).addClass('active');
+        localStorage.setItem('currentIndex', index);
+    }
+
+    let cachedIndex = localStorage.getItem('currentIndex');
+    if (cachedIndex) {
+        currentIndex = parseInt(cachedIndex);
+    }
+    showImage(currentIndex);
+
+    $('.prev').click(function () {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    });
+
+    $('.next').click(function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    });
+});
